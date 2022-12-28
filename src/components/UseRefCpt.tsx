fe294111fbeebbbf8c3b6477de8c81449a65f1f5
samy senani <aceportgasd235@gmail.com>
2022-12-28 09:43:19 +0100

    add: use ref hook

---
+++ src/components/UseRefCpt.tsx
@@ -0,0 +1,24 @@
+import { useRef, useState } from "react";
+
+function UseRefCpt() {
+  const [value, setValue] = useState<string | undefined>();
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  function focusInput(): void {
+    inputRef.current?.focus();
+  }
+  function valueInput(): void {
+    const value = inputRef.current?.value;
+    setValue(value);
+  }
+  //   console.log("ici");
+  return (
+    <div>
+      <input ref={inputRef} />
+      <button onClick={() => focusInput()}> focus</button>
+      <button onClick={() => valueInput()}> value</button>
+      <p>input value: {value} </p>
+    </div>
+  );
+}
+
+export default UseRefCpt;
