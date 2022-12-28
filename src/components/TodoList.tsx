840caedf9573ba6cdc9636ef0ede74dce0f12e2b
samy senani <aceportgasd235@gmail.com>
2022-12-28 08:56:20 +0100

    add: todo list

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,24 @@
+import { typeTodo } from "../App";
+import SingleTodo from "./SingleTodo";
+
+interface ITodoListProps {
+  todos: typeTodo[];
+  setTodos: React.Dispatch<React.SetStateAction<typeTodo[]>>;
+}
+export default function TodoList({ todos, setTodos }: ITodoListProps) {
+  const isEmpty = (data: any): boolean => {
+    if (Array.isArray(data) && data.length > 0) return false;
+    if (!Array.isArray(data) && Object.values(data).length > 0) return false;
+    if (!data) return false;
+    return true;
+  };
+  return (
+    <div>
+      {!isEmpty(todos)
+        ? todos?.map((todo: typeTodo) => (
+            <SingleTodo key={todo.id} todo={todo} setTodos={setTodos} />
+          ))
+        : "Todos Empty !"}
+    </div>
+  );
+}
