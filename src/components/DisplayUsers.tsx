1ba1b2da3a847917337e38c8b1c5c156b99c7369
samy senani <aceportgasd235@gmail.com>
2022-12-28 08:29:31 +0100

    add: display users example

---
+++ src/components/DisplayUsers.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+type userType = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+};
+export default function DisplayUsers() {
+  const [users, setUsers] = useState<userType[]>([]);
+
+  async function getUsers() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data: userType[] = await response.json();
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    // getUsers();
+  }, []);
+
+  return (
+    <div>
+      <h2> Users List </h2>
+      <>
+        {users.map((user: userType, idx: number) => (
+          <li key={idx}> {user?.username} </li>
+        ))}
+      </>
+    </div>
+  );
+}
