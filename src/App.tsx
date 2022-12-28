4fdc1eee974b3ab99a8c3d54e79774bc8e183c6b
samy senani <aceportgasd235@gmail.com>
2022-12-28 08:57:07 +0100

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DisplayUsers from "./components/DisplayUsers";
 import Count from "./components/Count";
+import TodoApp from "./components/TodoApp";
+
+export type typeTodo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 function App() {
   return (
@@ -14,51 +22,3 @@ function App() {
 }
 
 export default App;
-
-function TodoApp() {
-  const [todos, setTodos] = useState<Array<typeTodo>>([]);
-  return (
-    <div>
-      <h2> Todo App </h2>
-      <TodoList todos={todos} setTodos={setTodos} />
-    </div>
-  );
-}
-
-export type typeTodo = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
-
-interface ITodoListProps {
-  todos: typeTodo[];
-  setTodos: React.Dispatch<React.SetStateAction<typeTodo[]>>;
-}
-function TodoList({ todos, setTodos }: ITodoListProps) {
-  return (
-    <div>
-      {Array.isArray(todos) && todos.length > 0
-        ? todos?.map((todo: typeTodo) => (
-            <SingleTodo key={todo.id} todo={todo} setTodos={setTodos} />
-          ))
-        : "Todos Empty !"}
-    </div>
-  );
-}
-
-interface ISingleTodoProps {
-  todo: typeTodo;
-  setTodos: React.Dispatch<React.SetStateAction<typeTodo[]>>;
-}
-function SingleTodo({ todo, setTodos }: ISingleTodoProps) {
-  return (
-    <div>
-      <span> {todo?.id} </span>
-      <span> {todo?.title} </span>
-      <button onClick={() => {}}>Edit</button>
-      <button onClick={() => {}}>Delete</button>
-    </div>
-  );
-}
