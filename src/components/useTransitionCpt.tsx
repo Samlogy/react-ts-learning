1f785674c4bddfdec2ac0537ab6566fcd47fcc6e
samy senani <aceportgasd235@gmail.com>
2023-01-14 17:41:41 +0100

    add: useTransition hook example

---
+++ src/components/useTransitionCpt.tsx
@@ -0,0 +1,46 @@
+import React, { useTransition, useState } from "react";
+import { generateProducts } from "../helpers/functions";
+
+export default function UseTransitionCpt() {
+  const [isPending, startTransition] = useTransition();
+  const [filterTerm, setFilterTerm] = useState("");
+
+  const dummyProducts = generateProducts();
+
+  function filterProducts(filterTerm: string) {
+    if (!filterTerm) {
+      return dummyProducts;
+    }
+    return dummyProducts.filter((product) => product.includes(filterTerm));
+  }
+
+  const filteredProducts = filterProducts(filterTerm);
+
+  function updateFilterHandler(event: any) {
+    startTransition(() => {
+      setFilterTerm(event.target.value);
+      console.log("here");
+    });
+  }
+  console.log(isPending);
+
+  // useTransition: used specially for slow devices (the code is more responsive, less laggy)
+  // startTransition: wraps a function responsile for this behaviour (ex: filtering in our case)
+  // isPending: boolean value that tells us some state updates that are still pending (hasn't been yet by react) --> so we can show some fallback in the UI.
+
+  return (
+    <div id="app">
+      <input
+        type="text"
+        placeholder="filter term"
+        onChange={updateFilterHandler}
+      />
+      {isPending && <p style={{ color: "gray" }}>Updating list..</p>}
+      <ul>
+        {filteredProducts.map((product, idx) => (
+          <li key={idx}>{product}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
